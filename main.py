3ebba174c4c27676f269c3f2292fa742010a3401
zineb mouhib <zinebmouhib@hotmail.it>
2026-03-31 11:28:19 +0200

    Added: Transform,flag,reject

--- main.py
+++ main.py
@@ -1,9 +1,67 @@
 import pandas as pd 
 import os 
 
-# extract data from csv file
+# extract läser data från csv file
 df = pd.read_csv('data/products.csv', sep=';')
 print ("====== Data Extracted from CSV File ====")
 print (df.head())
-print ("\nKolumter: ", df.columns.tolist())
+print ("\nKolonner: ", df.columns.tolist())
+
+
+# transform - städning av data 
+
+
+df['name'] = df['name'].astype(str).str.strip()
+df['currency']= df['currency'].astype(str).str.strip()
+df['price']= pd.to_numeric(df['price'],errors='coerce')
+
+print ("\n====== Data Transformed ====")
+print (df.head())
+
+
+# flag -  möjliga problem med data
+
+# Saknar currency
+df['flag_missing_currency'] = df['currency'].isna()
+
+# Negativt pris
+df['flag_negative_price'] = df['price'] < 0
+
+# Extremt högt pris (t.ex. över 1000)
+df['flag_high_price'] = df['price']> 10000
+
+# Pris = 0
+df['flag_zero_price'] = df['price'] == 0
+# Null-ID, id som saknas eller är NaN
+df['flag_missing_id'] = df['id'].isna()
+
+print("\n==== >Falaggade produkter< ====")
+print("Saknar cyrrency:",df['flag_missing_currency'].sum())
+print("Negativt pris:", df['flag_negative_price'].sum())
+print("Extrem hög pris:", df['flag_high_price'].sum())
+print("Noll pris:", df['flag_zero_price'].sum())
+
+print("\nDEBUG:")
+print(df[['id','price','currency',
+          'flag_missing_currency',
+          'flag_negative_price',
+          'flag_high_price',
+          'flag_zero_price']])
+
+
+
+# ===== REJECT =====
+
+df_clean = df[
+    (df['flag_missing_currency'] == False) &
+    (df['flag_negative_price'] == False) &
+    (df['flag_zero_price'] == False)&
+    (df['flag_missing_id'] == False)
+]
+
+print("\n====== Clean Data (Efter Reject) ======")
+print(df_clean.head())
+print("\nAntal rader före:", len(df))
+print("Antal rader efter:", len(df_clean))
+
 
